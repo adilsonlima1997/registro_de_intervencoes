948bbe478dee357f68e7d6c074a180cb08281426
Adilson Lima <addy1997lima@gmail.com>
2024-01-21 13:58:04 -0100

    initial commit

---
+++ src/main/java/com/sal/grupo/oasis/registro_de_intervencoes/RegistroDeIntervencoesApplication.java
@@ -0,0 +1,13 @@
+package com.sal.grupo.oasis.registro_de_intervencoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroDeIntervencoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroDeIntervencoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sal/grupo/oasis/registro_de_intervencoes/RegistroDeIntervencoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sal.grupo.oasis.registro_de_intervencoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroDeIntervencoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
