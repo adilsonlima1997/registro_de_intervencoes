53ce7ca6a3a4b7d986856b9de8d93270b8123c4d
Adilson Lima <addy1997lima@gmail.com>
2024-01-21 22:35:40 -0100

    21-01-2024

--- src/main/java/com/sal/grupo/oasis/registro_de_intervencoes/Controller/Controle.java
+++ src/main/java/com/sal/grupo/oasis/registro_de_intervencoes/Controller/Controle.java
@@ -1,5 +1,13 @@
 package com.sal.grupo.oasis.registro_de_intervencoes.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controle {
     
+    @GetMapping("/")
+    public String teste(){
+        return "Hello World";
+    }
 }
---
+++ src/main/java/com/sal/grupo/oasis/registro_de_intervencoes/Model/Avarias.java
@@ -0,0 +1,5 @@
+package com.sal.grupo.oasis.registro_de_intervencoes.Model;
+
+public class Avarias {
+    
+}
