0b4443aaf248937e8fc56ad8dd07ec37c6ed5c94
Adilson Lima <addy1997lima@gmail.com>
2024-01-22 13:15:42 -0100

    22-01-2024

--- src/main/java/com/sal/grupo/oasis/registro_de_intervencoes/Controller/Controle.java
+++ src/main/java/com/sal/grupo/oasis/registro_de_intervencoes/Controller/Controle.java
@@ -2,6 +2,7 @@ package com.sal.grupo.oasis.registro_de_intervencoes.Controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.sal.grupo.oasis.registro_de_intervencoes.Model.Avarias;
 import com.sal.grupo.oasis.registro_de_intervencoes.Repository.Repositorio;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class Controle {
     
     @Autowired
