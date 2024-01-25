99f6da8609896245ec8ea789f2fa8ab685ff9b66
Adilson Lima <addy1997lima@gmail.com>
2024-01-25 20:15:10 -0100

    25-01-2024

--- src/main/java/com/sal/grupo/oasis/registro_de_intervencoes/Model/Avarias.java
+++ src/main/java/com/sal/grupo/oasis/registro_de_intervencoes/Model/Avarias.java
@@ -1,7 +1,7 @@
 package com.sal.grupo.oasis.registro_de_intervencoes.Model;
 
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
